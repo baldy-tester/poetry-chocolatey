1fda7b1ff4205982d59bbd5e70592a270ed2e452
BaldyTester <4173351+BaldyTester@users.noreply.github.com>
2020-05-21 09:05:30 -0400

    Changed file encoding

--- tools/chocolateyuninstall.ps1
+++ tools/chocolateyuninstall.ps1
@@ -1,4 +1,4 @@
-﻿$ErrorActionPreference = 'Stop'; # stop on all errors
+$ErrorActionPreference = 'Stop'; # stop on all errors
 $toolsDir         = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $uninstallCommand = "python  $toolsDir\get-poetry.py --uninstall -y"
 Start-ChocolateyProcessAsAdmin $uninstallCommand
