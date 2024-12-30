824b93063d63cc07618ebc1d688d41dbb476c0fb
MarthinusBosman <marthinusbosman@gmail.com>
2024-12-30 12:42:35 +0100

    Fix version and use new poetry install URL

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -1,5 +1,17 @@
 $ErrorActionPreference = 'Stop'; # stop on all errors
-$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py' # download url, HTTPS preferred
-$command    = "(Invoke-WebRequest -Uri $url -UseBasicParsing -OutFile $toolsDir\get-poetry.py) | python $toolsDir\get-poetry.py"
+
+# Define the variables for the installation
+$packageName = 'poetry'
+$url = 'https://install.python-poetry.org'
+$filePath = Join-Path $env:TEMP 'install-poetry.py'
+$version = '1.8.5'
+
+# Download the installation script using Chocolatey helper
+Get-ChocolateyWebFile -packageName $packageName -Url $url -FileFullPath $filePath
+
+# Run the downloaded script using Python
+$command = "py `"$filePath`" --version $version"
 Start-ChocolateyProcessAsAdmin $command
+
+# Clean up the installation file after installation
+Remove-Item $filePath -Force
--- tools/chocolateyuninstall.ps1
+++ tools/chocolateyuninstall.ps1
@@ -1,4 +1,18 @@
 $ErrorActionPreference = 'Stop'; # stop on all errors
-$toolsDir         = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$uninstallCommand = "python  $toolsDir\get-poetry.py --uninstall -y"
+
+$ErrorActionPreference = 'Stop'; # stop on all errors
+
+# Define the variables for the installation
+$packageName = 'poetry'
+$url = 'https://install.python-poetry.org'
+$filePath = Join-Path $env:TEMP 'install-poetry.py'
+
+# Download the installation script using Chocolatey helper
+Get-ChocolateyWebFile -packageName $packageName -Url $url -FileFullPath $filePath
+
+# Run the downloaded script using Python
+$uninstallCommand = "py `"$filePath`" --uninstall"
 Start-ChocolateyProcessAsAdmin $uninstallCommand
+
+# Clean up the installation file after installation
+Remove-Item $filePath -Force
